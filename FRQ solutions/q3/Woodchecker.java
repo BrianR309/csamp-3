a172e67f5f0ac6ace0f49f998df233af946b0eac
BrianR309 <BrianR309@nycstudents.net>
2024-05-22 11:46:56 -0400

    Update Woodchecker.java

--- FRQ solutions/q3/Woodchecker.java
+++ FRQ solutions/q3/Woodchecker.java
@@ -1,1 +1,22 @@
 
+import java.util.*;
+
+public class WordChecker {
+  private ArrayList<String> wordList;
+
+  public WordChecker(ArrayList<String> words) {
+    wordList = words;
+  }
+
+  public boolean isWordChain() {
+
+    return false;
+  }
+
+
+  public ArrayList<String> createList(String target) {
+    ArrayList<String> result = new ArrayList<String>();
+
+    return result;             
+  }
+}
