a70a7d4d80708dc264808682cc22e831a20af6f8
BrianR309 <BrianR309@nycstudents.net>
2024-05-22 11:45:18 -0400

    Update Gridlock.java

--- FRQ solutions/q4/Gridlock.java
+++ FRQ solutions/q4/Gridlock.java
@@ -1,1 +1,32 @@
 
+import java.util.Arrays;
+
+public class GridPath {
+  private int[][] grid;
+
+  public GridPath(int[][] grid) {
+    this.grid = grid;
+  }
+
+  
+ public Location getNextLoc(int row, int col) {
+
+   Location rightLoc = new Location(row, col +1);
+   Location belowLoc = new Location(row+1,col);
+  
+
+  
+   if(col == grid[0].length -1){
+     return belowLoc;
+   }
+   if(row == grid.length-1){
+      return rightLoc;
+    }
+   if(grid[row+1][col] < grid[row][col+1]){
+     return belowLoc;
+   }
+   else{
+     return rightLoc;
+   }
+ 
+ }
